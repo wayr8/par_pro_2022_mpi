9910a2d1e7d87c60d50e1805f3de956d7a54e4c9
blast-sky <strogancev2020@gmail.com>
2022-11-13 14:49:40 +0300

    replace tab on spaces

--- modules/task_1/strogantsev_a_matrix_column_sum/matrix_column_sum.h
+++ modules/task_1/strogantsev_a_matrix_column_sum/matrix_column_sum.h
@@ -10,11 +10,11 @@ int linearizeCoordinates(int x, int y, int xSize);
 std::vector<int> calculateMatrixSumParallel(const std::vector<int>& matrix, int xSize, int ySize);
 std::vector<int> calculateMatrixSumSequentially(const std::vector<int>& matrix, int xSize, int ySize);
 std::vector<int> calculateMatrixSumSequentially(
-	const std::vector<int>& matrix,
-	int xSize,
-	int ySize,
-	int fromX,
-	int toX
+    const std::vector<int>& matrix,
+    int xSize,
+    int ySize,
+    int fromX,
+    int toX
 );
 
 #endif  // MODULES_TASK_1_STROGANTSEV_A_MATRIX_COLUMN_SUM_MATRIX_COLUMN_SUM_H_
