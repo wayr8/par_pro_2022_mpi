fac39bd787ae15325194d0763fc47b7b1ff99af2
AleksandrNikolaev02 <83976322+AleksandrNikolaev02@users.noreply.github.com>
2022-11-14 10:27:21 +0300

    init

--- modules/task_1/nikolaev_a_vector_average/main.cpp
+++ modules/task_1/nikolaev_a_vector_average/main.cpp
@@ -76,6 +76,24 @@ TEST(Vector_Average_MPI, Test_Average4) {
     }
 }
 
+TEST(Vector_Average_MPI, Test_Average5) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> global_vec;
+    const int count_size_vector = 1000000;
+
+    if (rank == 0) {
+        global_vec = getRandomVector(count_size_vector);
+    }
+
+    int parallel_average = getAverageVectorParallel(global_vec);
+
+    if (rank == 0) {
+        int sequential_average = getAverageVectorSequential(global_vec);
+        ASSERT_EQ(sequential_average, parallel_average);
+    }
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
