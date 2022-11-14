03b8d4c0ea1bbdc92ebd4fd5491054253cff4654
AleksandrNikolaev02 <alnikolaev151@yandex.ru>
2022-11-14 14:17:09 +0300

    init

--- modules/task_1/nikolaev_a_vector_average/vector_average.cpp
+++ modules/task_1/nikolaev_a_vector_average/vector_average.cpp
@@ -27,52 +27,53 @@ int getAverageVectorSequential(std::vector<int> vec) {
 }
 
 int getAverageVectorParallel(std::vector<int> vec) {
-    // int size, rank, sum=0, n, i, ibeg, iend;
-    // MPI_Comm_size(MPI_COMM_WORLD, &size);
-    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    // const int SizeVector = vec.size();
-
-    // n = (SizeVector - 1) / size + 1;
-    // ibeg = rank*n;
-    // iend = (rank + 1) * n;
-
-    // if(rank != FIRST_THREAD) {
-    //     MPI_Status status;
-    //     MPI_Recv(&sum, 1, MPI_INT, rank-1, 10, MPI_COMM_WORLD, &status);
-    // }
-
-    // for(i=ibeg; i<((iend>SizeVector)?SizeVector:iend); i++) {
-    //   sum = sum + vec[i];
-    // }
-
-    // if(rank != size-1) {
-    //     MPI_Send(&sum, 1, MPI_INT, rank + 1, 10, MPI_COMM_WORLD);
-    // }
-
-    int size, rank;
+    int size, rank, sum=0, n, i, ibeg, iend;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    const int delta = vec.size() / size;
+    const int SizeVector = vec.size();
 
-    if (rank == 0) {
-        for (int proc = 1; proc < size; proc++) {
-            MPI_Send(vec.data() + proc * delta, delta,
-                        MPI_INT, proc, 0, MPI_COMM_WORLD);
-        }
-    }
+    n = (SizeVector - 1) / size + 1;
+    ibeg = rank*n;
+    iend = (rank + 1) * n;
 
-    std::vector<int> local_vec(delta);
-    if (rank == 0) {
-        local_vec = std::vector<int>(vec.begin(),
-                                     vec.begin() + delta);
-    } else {
+    if(rank != FIRST_THREAD) {
         MPI_Status status;
-        MPI_Recv(local_vec.data(), delta, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&sum, 1, MPI_INT, rank-1, 10, MPI_COMM_WORLD, &status);
     }
 
-    int global_sum = 0;
-    int local_average = getAverageVectorSequential(local_vec);
-    MPI_Reduce(&local_average, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    for(i=ibeg; i<((iend>SizeVector)?SizeVector:iend); i++) {
+      sum = sum + vec[i];
+    }
 
-    return global_sum/vec.size();
+    if(rank != size-1) {
+        MPI_Send(&sum, 1, MPI_INT, rank + 1, 10, MPI_COMM_WORLD);
+    }
+
+    // int size, rank;
+    // MPI_Comm_size(MPI_COMM_WORLD, &size);
+    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // const int delta = vec.size() / size;
+
+    // if (rank == 0) {
+    //     for (int proc = 1; proc < size; proc++) {
+    //         MPI_Send(vec.data() + proc * delta, delta,
+    //                     MPI_INT, proc, 0, MPI_COMM_WORLD);
+    //     }
+    // }
+
+    // std::vector<int> local_vec(delta);
+    // if (rank == 0) {
+    //     local_vec = std::vector<int>(vec.begin(),
+    //                                  vec.begin() + delta);
+    // } else {
+    //     MPI_Status status;
+    //     MPI_Recv(local_vec.data(), delta, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+    // }
+
+    // int global_sum = 0;
+    // int local_average = getAverageVectorSequential(local_vec);
+    // MPI_Reduce(&local_average, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    // return global_sum/vec.size();
+    return sum / SizeVector;
 }
