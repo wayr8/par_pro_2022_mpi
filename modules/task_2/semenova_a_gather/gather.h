fe35310f2c31c508547117b3494f9f562da5a45b
nika-semen <nika-semen@mail.ru>
2022-12-01 23:51:38 +0300

    Att№5

---
+++ modules/task_2/semenova_a_gather/gather.h
@@ -0,0 +1,17 @@
+// Copyright 2022 Semenova Veronika
+#ifndef MODULES_TASK_2_SEMENOVA_A_GATHER_GATHER_H_
+#define MODULES_TASK_2_SEMENOVA_A_GATHER_GATHER_H_
+#pragma once
+#include <mpi.h>
+
+#include<random>
+
+template < typename Type >
+void RandVec(Type* A, int n) {
+    for (int i = 0; i < n; i++)
+        A[i] = Type(rand() * ((double)rand() / (double)RAND_MAX - 0.5));
+}
+
+int Gather(void* sbuf, int scount, MPI_Datatype stype, void* rbuf, int rcount, MPI_Datatype rtype, int root, MPI_Comm comm);
+
+#endif // MODULES_TASK_2_SEMENOVA_A_GATHER_GATHER_H_ 
