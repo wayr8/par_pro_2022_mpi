aca8547509faed6b03197be35d450ef755563c24
Aleksandr Eremin <aleksandr_ereminn@mail.ru>
2022-12-03 11:42:57 +0300

    test

---
+++ modules/task_1/eremin_a_vector_sum/ops_mpi.h
@@ -0,0 +1,14 @@
+
+#ifndef MODULES_TASK_1_EREMIN_A_VECTOR_SUM_VECSUM_H_
+#define MODULES_TASK_1_EREMIN_A_VECTOR_SUM_VECSUM_H_
+
+#include <vector>
+#include <string>
+
+std::vector<int> random(int  size);
+
+int sum(std::vector<int> V);
+int sumParallel(std::vector<int> Vector, int size);
+
+
+#endif  // MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
