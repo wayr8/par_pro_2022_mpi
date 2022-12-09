fc76cf32979b59efe6651716a57df8507dd181eb
Aleksandr Eremin <aleksandr_ereminn@mail.ru>
2022-12-09 15:11:14 +0300

    test1

---
+++ modules/task_2/eremin_a_sleeping_barber/ops_mpi.cpp
@@ -0,0 +1,88 @@
+// Copyright 2022 Eremin Aleksandr
+#include <math.h>
+#include <mpi.h>
+#include <thread>
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <queue>
+#include "../../../modules/test_tasks/test_mpi/ops_mpi.h"
+
+#define Get_in_line 1
+#define Go_Barber 2
+#define Get_un_line 3
+#define Exit_Queue 4
+
+void wait() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    int index = (200 + gen() % 200);
+    std::this_thread::sleep_for(std::chrono::milliseconds(index));
+}
+
+void client(int rank) {
+    wait();
+    MPI_Status status;
+    int queue_index = 0;
+
+    MPI_Send(&rank, 1, MPI_INT, 1, Get_in_line, MPI_COMM_WORLD);
+    MPI_Recv(&queue_index, 1, MPI_INT, MPI_ANY_SOURCE, Get_un_line,
+        MPI_COMM_WORLD, &status);
+
+    if (queue_index != 0) {
+        MPI_Send(&rank, 1, MPI_INT, 0, Go_Barber, MPI_COMM_WORLD);
+    }
+    else {
+        int nun = -1;
+        MPI_Send(&nun, 1, MPI_INT, 0, Go_Barber, MPI_COMM_WORLD);
+    }
+}
+
+void queue(int numberOFseats, int numberOFclients) {
+    int client_rank;
+    int queue_index = 0, index = 0;
+    MPI_Status status;
+    std::queue<int> same_queue;
+    int i = 0;
+    while (i < numberOFclients) {
+        MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        if (status.MPI_TAG != Exit_Queue && index < numberOFclients) {
+            MPI_Recv(&client_rank, 1, MPI_INT, MPI_ANY_SOURCE, Get_in_line,
+                MPI_COMM_WORLD, &status);
+            if ((same_queue.size()) < numberOFseats) {
+                same_queue.push(client_rank);
+                queue_index = 1;
+                index++;
+                MPI_Send(&queue_index, 1, MPI_INT, client_rank, Get_un_line,
+                    MPI_COMM_WORLD);
+            }
+            else {
+                queue_index = 0;
+                MPI_Send(&queue_index, 1, MPI_INT, client_rank, Get_un_line,
+                    MPI_COMM_WORLD);
+                i++;
+                index++;
+            }
+        }
+        if (status.MPI_TAG == Exit_Queue || index == numberOFclients) {
+            MPI_Recv(&client_rank, 1, MPI_INT, MPI_ANY_SOURCE, Exit_Queue,
+                MPI_COMM_WORLD, &status);
+            same_queue.pop();
+            i++;
+        }
+    }
+}
+
+void Barber(int numberOFseats, int numberOFclients) {
+    int client_rank = 0;
+    MPI_Status status;
+    for (int i = 0; i < numberOFclients; i++) {
+        MPI_Recv(&client_rank, 1, MPI_INT, MPI_ANY_SOURCE, Go_Barber,
+            MPI_COMM_WORLD, &status);
+        if (client_rank != -1) {
+            MPI_Send(&client_rank, 1, MPI_INT, 1, Exit_Queue, MPI_COMM_WORLD);
+            wait();
+        }
+    }
+}
---
+++ modules/task_2/eremin_a_sleeping_barber/ops_mpi.h
@@ -0,0 +1,12 @@
+// Copyright 2022 Eremin Aleksandr
+#ifndef MODULES_TASK_1_EREMIN_A_VECTOR_SUM_OPS_MPI_H_
+#define MODULES_TASK_1_EREMIN_A_VECTOR_SUM_OPS_MPI_H_
+
+#include <vector>
+#include <string>
+
+void client(int rank);
+void queue(int numberOFseats, int numberOFclients);
+void Barber(int numberOFseats, int numberOFclients);
+
+#endif  // MODULES_TASK_1_EREMIN_A_VECTOR_SUM_OPS_MPI_H_
