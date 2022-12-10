94b3977d751acabb794b0035706bfb4fa6a60973
Alexandr <poolce@list.ru>
2022-12-10 21:15:07 +0300

    musin_a_dining_philosophers

--- modules/task_2/musin_a_dining_philosophers/main.cpp
+++ modules/task_2/musin_a_dining_philosophers/main.cpp
@@ -1,105 +1,98 @@
-// Copyright 2018 Nesterov Alexander
+// Copyright 2021 Musin Alexandr
 #include <gtest/gtest.h>
-#include "./dining_philosophers.h"
+
 #include <gtest-mpi-listener.hpp>
 
+#include "./dining_philosophers.h"
 
- TEST(Parallel_Operations_MPI, Test_Plus_0) {
-     int incriment_parameter = 0;
-     int rank;
-     int size;
+TEST(Parallel_Operations_MPI, Test_Plus_0) {
+    int incriment_parameter = 0;
+    int rank;
+    int size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     if (rank == 0) {
-    int res = manager(size);
-    ASSERT_EQ(res,incriment_parameter*(size-1));
+        int res = manager(size);
+        ASSERT_EQ(res, incriment_parameter * (size - 1));
+    } else {
+        philosoph(rank, incriment_parameter);
     }
-    else {
-        philosoph(rank,incriment_parameter);
-    }
- } 
+}
 
- TEST(Parallel_Operations_MPI, Test_Minus_1) {
-     int incriment_parameter = -1;
-     int rank;
-     int size;
+TEST(Parallel_Operations_MPI, Test_Minus_1) {
+    int incriment_parameter = -1;
+    int rank;
+    int size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     if (rank == 0) {
-    int res = manager(size);
-    ASSERT_EQ(res,incriment_parameter*(size-1));
+        int res = manager(size);
+        ASSERT_EQ(res, incriment_parameter * (size - 1));
+    } else {
+        philosoph(rank, incriment_parameter);
     }
-    else {
-        philosoph(rank,incriment_parameter);
-    }
- } 
+}
 
-  TEST(Parallel_Operations_MPI, Test_Plus_2) {
-     int incriment_parameter = 2;
-     int rank;
-     int size;
+TEST(Parallel_Operations_MPI, Test_Plus_2) {
+    int incriment_parameter = 2;
+    int rank;
+    int size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     if (rank == 0) {
-    int res = manager(size);
-    ASSERT_EQ(res,incriment_parameter*(size-1));
+        int res = manager(size);
+        ASSERT_EQ(res, incriment_parameter * (size - 1));
+    } else {
+        philosoph(rank, incriment_parameter);
     }
-    else {
-        philosoph(rank,incriment_parameter);
-    }
-  }  
-  
-  TEST(Parallel_Operations_MPI, Tes_Plus_3) {
-     int incriment_parameter = 3;
-     int rank;
-     int size;
+}
+
+TEST(Parallel_Operations_MPI, Tes_Plus_3) {
+    int incriment_parameter = 3;
+    int rank;
+    int size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     if (rank == 0) {
-    int res = manager(size);
-    ASSERT_EQ(res,incriment_parameter*(size-1));
+        int res = manager(size);
+        ASSERT_EQ(res, incriment_parameter * (size - 1));
+    } else {
+        philosoph(rank, incriment_parameter);
     }
-    else {
-        philosoph(rank,incriment_parameter);
-    }
-  }
+}
 
-  TEST(Parallel_Operations_MPI, Test_Plus_And_Minus) {
-     int rank;
-     int size;
+TEST(Parallel_Operations_MPI, Test_Plus_And_Minus) {
+    int rank;
+    int size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int examination = size%2==0 ? -1:0;
+    int examination = size % 2 == 0 ? -1 : 0;
 
     if (rank == 0) {
-    int res = manager(size);
-    ASSERT_EQ(res,examination);
-    }
-    else {  
-        if(rank%2==0)
-        {
-            philosoph(rank,1);
+        int res = manager(size);
+        ASSERT_EQ(res, examination);
+    } else {
+        if (rank % 2 == 0) {
+            philosoph(rank, 1);
         }
-        if(rank%2!=0)
-        {
-            philosoph(rank,-1);
+        if (rank % 2 != 0) {
+            philosoph(rank, -1);
         }
     }
-  }
+}
 
- int main(int argc, char** argv) {
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
 
-     ::testing::InitGoogleTest(&argc, argv);
-     MPI_Init(&argc, &argv);
- 
-     ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-     ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners &listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
 
-     listeners.Release(listeners.default_result_printer());
-     listeners.Release(listeners.default_xml_generator());
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
 
-     listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-     return RUN_ALL_TESTS();
- }
- 
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
