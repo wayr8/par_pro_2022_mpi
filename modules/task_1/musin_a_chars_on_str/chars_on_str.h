60e03bdc0b7d305c1778a22f16bad1a619d3c15d
Alexandr <poolce@list.ru>
2022-12-10 18:18:24 +0300

    musin_a_chars_on_str

---
+++ modules/task_1/musin_a_chars_on_str/chars_on_str.h
@@ -0,0 +1,11 @@
+#ifndef MODULES_TASK_1_MUSIN_A_CHARS_ON_STR_H_
+#define MODULES_TASK_1_MUSIN_A_CHARS_ON_STR_H_
+
+#include <string>
+
+char* getRandomString(const int size);
+int par_sym_on_str(const char* global_str, const int global_str_len, const char sym);
+int sym_on_str(const char* str,const int size,const char sym);
+
+
+#endif
