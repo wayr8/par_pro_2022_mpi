24b3bc3b53c7b169edad53db8fbc5270b98bb5b7
Alexandr <poolce@list.ru>
2022-12-10 20:04:04 +0300

    musin_a_chars_on_str

--- modules/task_1/musin_a_chars_on_str/chars_on_str.h
+++ modules/task_1/musin_a_chars_on_str/chars_on_str.h
@@ -1,11 +1,12 @@
-#ifndef MODULES_TASK_1_MUSIN_A_CHARS_ON_STR_H_
-#define MODULES_TASK_1_MUSIN_A_CHARS_ON_STR_H_
+// Copyright 2021 Musin Alexandr
+#ifndef MODULES_TASK_1_MUSIN_A_CHARS_ON_STR_CHARS_ON_STR_H_
+#define MODULES_TASK_1_MUSIN_A_CHARS_ON_STR_CHARS_ON_STR_H_
 
 #include <string>
 
-char* getRandomString(const int size);
-int par_sym_on_str(const char* global_str, const int global_str_len, const char sym);
-int sym_on_str(const char* str,const int size,const char sym);
+char *getRandomString(const int size);
+int par_sym_on_str(const char *global_str, const int global_str_len,
+                   const char sym);
+int sym_on_str(const char *str, const int size, const char sym);
 
-
-#endif
+#endif  // MODULES_TASK_1_MUSIN_A_CHARS_ON_STR_CHARS_ON_STR_H_
