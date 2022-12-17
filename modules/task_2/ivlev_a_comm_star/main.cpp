a0360be561cc7f75e121b22c4be2fc473756793d
Faert <ivlev-sd@mail.ru>
2022-12-17 10:57:12 +0300

    test0_1

--- modules/task_2/ivlev_a_comm_star/main.cpp
+++ modules/task_2/ivlev_a_comm_star/main.cpp
@@ -12,26 +12,27 @@ TEST(Test_comm_star_MPI, Test_0) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    MPI_Comm star_comm;
-    int index[4] = {0, 1, 2, 3};
-    MPI_group_star_create(MPI_COMM_WORLD, 4,
-        index, &star_comm);
+    if (size >= 4) {
+        MPI_Comm star_comm;
+        int index[4] = {0, 1, 2, 3};
+        MPI_group_star_create(MPI_COMM_WORLD, 4,
+            index, &star_comm);
 
-    int star_rank, star_size;
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_rank(star_comm, &star_rank);
-        MPI_Comm_size(star_comm, &star_size);
+        int star_rank, star_size;
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_rank(star_comm, &star_rank);
+            MPI_Comm_size(star_comm, &star_size);
 
-        EXPECT_LT(star_rank, star_size);
-        EXPECT_LT(rank, 4);
-        EXPECT_GE(rank, 0);
-    } else {
-        star_rank = -1;
-        star_size = -1;
-    }
+            EXPECT_LT(star_rank, star_size);
+            EXPECT_LT(rank, 4);
+        } else {
+            star_rank = -1;
+            star_size = -1;
+        }
 
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_free(&star_comm);
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_free(&star_comm);
+        }
     }
 }
 
@@ -41,41 +42,43 @@ TEST(Test_comm_star_MPI, Test_1) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    MPI_Comm star_comm;
-    int index[4] = {0, 1, 2, 3};
-    MPI_group_star_create(MPI_COMM_WORLD, 4,
-        index, &star_comm);
+    if (size >= 4) {
+        MPI_Comm star_comm;
+        int index[4] = {0, 1, 2, 3};
+        MPI_group_star_create(MPI_COMM_WORLD, 4,
+            index, &star_comm);
 
-    int star_rank, star_size;
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_rank(star_comm, &star_rank);
-        MPI_Comm_size(star_comm, &star_size);
+        int star_rank, star_size;
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_rank(star_comm, &star_rank);
+            MPI_Comm_size(star_comm, &star_size);
 
-        int a[3];
+            int a[3];
 
-        if (star_rank == 3) {
-            for (int i = 0; i < 3; i++) {
-                a[i] = i;
+            if (star_rank == 3) {
+                for (int i = 0; i < 3; i++) {
+                    a[i] = i;
+                }
             }
+
+            Star_Send(a, 3, MPI_INT, 3, 1, 1, star_comm);
+
+            if (star_rank == 1) {
+                int b[3];
+                MPI_Status status;
+                MPI_Recv(b, 3, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
+                for (int i = 0; i < 3; i++) {
+                    EXPECT_EQ(b[i], i);
+                }
+            }
+        } else {
+            star_rank = -1;
+            star_size = -1;
         }
 
-        Star_Send(a, 3, MPI_INT, 3, 1, 1, star_comm);
-
-        if (star_rank == 1) {
-            int b[3];
-            MPI_Status status;
-            MPI_Recv(b, 3, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
-            for (int i = 0; i < 3; i++) {
-                EXPECT_EQ(b[i], i);
-            }
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_free(&star_comm);
         }
-    } else {
-        star_rank = -1;
-        star_size = -1;
-    }
-
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_free(&star_comm);
     }
 }
 
@@ -85,41 +88,43 @@ TEST(Test_comm_star_MPI, Test_2) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    MPI_Comm star_comm;
-    int index[4] = {0, 1, 2, 3};
-    MPI_group_star_create(MPI_COMM_WORLD, 4,
-        index, &star_comm);
+    if (size >= 4) {
+        MPI_Comm star_comm;
+        int index[4] = {0, 1, 2, 3};
+        MPI_group_star_create(MPI_COMM_WORLD, 4,
+            index, &star_comm);
 
-    int star_rank, star_size;
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_rank(star_comm, &star_rank);
-        MPI_Comm_size(star_comm, &star_size);
+        int star_rank, star_size;
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_rank(star_comm, &star_rank);
+            MPI_Comm_size(star_comm, &star_size);
 
-        int a[3];
+            int a[3];
 
-        if (star_rank == 2) {
-            for (int i = 0; i < 3; i++) {
-                a[i] = i;
+            if (star_rank == 2) {
+                for (int i = 0; i < 3; i++) {
+                    a[i] = i;
+                }
             }
+
+            Star_Send(a, 3, MPI_INT, 2, 0, 1, star_comm);
+
+            if (star_rank == 0) {
+                int b[3];
+                MPI_Status status;
+                MPI_Recv(b, 3, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
+                for (int i = 0; i < 3; i++) {
+                    EXPECT_EQ(b[i], i);
+                }
+            }
+        } else {
+            star_rank = -1;
+            star_size = -1;
         }
 
-        Star_Send(a, 3, MPI_INT, 2, 0, 1, star_comm);
-
-        if (star_rank == 0) {
-            int b[3];
-            MPI_Status status;
-            MPI_Recv(b, 3, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
-            for (int i = 0; i < 3; i++) {
-                EXPECT_EQ(b[i], i);
-            }
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_free(&star_comm);
         }
-    } else {
-        star_rank = -1;
-        star_size = -1;
-    }
-
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_free(&star_comm);
     }
 }
 
@@ -129,41 +134,43 @@ TEST(Test_comm_star_MPI, Test_3) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    MPI_Comm star_comm;
-    int index[4] = {0, 1, 2, 3};
-    MPI_group_star_create(MPI_COMM_WORLD, 4,
-        index, &star_comm);
+    if (size >= 4) {
+        MPI_Comm star_comm;
+        int index[4] = {0, 1, 2, 3};
+        MPI_group_star_create(MPI_COMM_WORLD, 4,
+            index, &star_comm);
 
-    int star_rank, star_size;
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_rank(star_comm, &star_rank);
-        MPI_Comm_size(star_comm, &star_size);
+        int star_rank, star_size;
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_rank(star_comm, &star_rank);
+            MPI_Comm_size(star_comm, &star_size);
 
-        int a[10];
+            int a[10];
 
-        if (star_rank == 2) {
-            for (int i = 0; i < 10; i++) {
-                a[i] = i;
+            if (star_rank == 2) {
+                for (int i = 0; i < 10; i++) {
+                    a[i] = i;
+                }
             }
+
+            Star_Send(a, 4, MPI_INT, 2, 1, 1, star_comm);
+
+            if (star_rank == 1) {
+                int b[4];
+                MPI_Status status;
+                MPI_Recv(b, 4, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
+                for (int i = 0; i < 4; i++) {
+                    EXPECT_EQ(b[i], i);
+                }
+            }
+        } else {
+            star_rank = -1;
+            star_size = -1;
         }
 
-        Star_Send(a, 4, MPI_INT, 2, 1, 1, star_comm);
-
-        if (star_rank == 1) {
-            int b[4];
-            MPI_Status status;
-            MPI_Recv(b, 4, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
-            for (int i = 0; i < 4; i++) {
-                EXPECT_EQ(b[i], i);
-            }
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_free(&star_comm);
         }
-    } else {
-        star_rank = -1;
-        star_size = -1;
-    }
-
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_free(&star_comm);
     }
 }
 
@@ -173,52 +180,54 @@ TEST(Test_comm_star_MPI, Test_4) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    MPI_Comm star_comm;
-    int index[4] = {0, 1, 2, 3};
-    MPI_group_star_create(MPI_COMM_WORLD, 4,
-        index, &star_comm);
+    if (size >= 4) {
+        MPI_Comm star_comm;
+        int index[4] = {0, 1, 2, 3};
+        MPI_group_star_create(MPI_COMM_WORLD, 4,
+            index, &star_comm);
 
-    int star_rank, star_size;
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_rank(star_comm, &star_rank);
-        MPI_Comm_size(star_comm, &star_size);
+        int star_rank, star_size;
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_rank(star_comm, &star_rank);
+            MPI_Comm_size(star_comm, &star_size);
 
-        int a[10];
-        int b[10];
+            int a[10];
+            int b[10];
 
-        if (star_rank == 2) {
-            for (int i = 0; i < 10; i++) {
-                a[i] = i;
+            if (star_rank == 2) {
+                for (int i = 0; i < 10; i++) {
+                    a[i] = i;
+                }
             }
+
+            Star_Send(a, 10, MPI_INT, 2, 1, 1, star_comm);
+
+            if (star_rank == 1) {
+                MPI_Status status;
+                MPI_Recv(b, 10, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
+                for (int i = 0; i < 10; i++) {
+                    EXPECT_EQ(b[i], i);
+                }
+            }
+
+            Star_Send(b, 3, MPI_INT, 1, 0, 1, star_comm);
+
+            if (star_rank == 0) {
+                int c[3];
+                MPI_Status status;
+                MPI_Recv(c, 3, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
+                for (int i = 0; i < 3; i++) {
+                    EXPECT_EQ(c[i], i);
+                }
+            }
+        } else {
+            star_rank = -1;
+            star_size = -1;
         }
 
-        Star_Send(a, 10, MPI_INT, 2, 1, 1, star_comm);
-
-        if (star_rank == 1) {
-            MPI_Status status;
-            MPI_Recv(b, 10, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
-            for (int i = 0; i < 10; i++) {
-                EXPECT_EQ(b[i], i);
-            }
+        if (star_comm != MPI_COMM_NULL) {
+            MPI_Comm_free(&star_comm);
         }
-
-        Star_Send(b, 3, MPI_INT, 1, 0, 1, star_comm);
-
-        if (star_rank == 0) {
-            int c[3];
-            MPI_Status status;
-            MPI_Recv(c, 3, MPI_INT, MPI_ANY_SOURCE, 1, star_comm, &status);
-            for (int i = 0; i < 3; i++) {
-                EXPECT_EQ(c[i], i);
-            }
-        }
-    } else {
-        star_rank = -1;
-        star_size = -1;
-    }
-
-    if (star_comm != MPI_COMM_NULL) {
-        MPI_Comm_free(&star_comm);
     }
 }
 
