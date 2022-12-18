51f5393d4c048d6e3b4a7a8c29446d45af00df81
wellcome2AD <iuuyyttttt@mail.ru>
2022-12-18 17:31:09 +0300

    dummy tests have been added

--- modules/task_1/korobeynikova_a_max_by_rows/main.cpp
+++ modules/task_1/korobeynikova_a_max_by_rows/main.cpp
@@ -12,7 +12,7 @@
 TEST(Parallel_Operations_MPI, Test_Max) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  const int n_size_matrix = 2, m_size_matrix = 3;
+  const int n_size_matrix = 5, m_size_matrix = 3;
   Matrix<int> global_matr(n_size_matrix, m_size_matrix);
 
   if (rank == 0) {
@@ -37,6 +37,31 @@ TEST(Parallel_Operations_MPI, Test_Max) {
 TEST(Parallel_Operations_MPI, Test_Max_2) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  const int n_size_matrix = 5, m_size_matrix = 3;
+  Matrix<int> global_matr(n_size_matrix, m_size_matrix);
+
+  if (rank == 0) {
+    global_matr = getRandomMatrix(n_size_matrix, m_size_matrix);
+    std::ofstream outf("C:/Users/User/Desktop/process_input.txt",
+                       std::ios_base::app);
+    outf << vtos(global_matr.GetData()) << std::endl;
+  }
+
+  std::vector<int> max_values;
+  max_values = getParallelOperation(global_matr);
+  if (rank == 0) {
+    std::vector<int> seq_max_values(n_size_matrix);
+    for (int i = 0; i < n_size_matrix; ++i) {
+      seq_max_values[i] =
+          findMax(std::vector<int>(global_matr[i], global_matr[i + 1]));
+    }
+    ASSERT_EQ(max_values, seq_max_values);
+  }
+}
+
+TEST(Parallel_Operations_MPI, Test_Max_3) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   std::vector<std::vector<int>> global_matr;
   const int n_size_matrix = 5, m_size_matrix = 8;
 
@@ -52,6 +77,49 @@ TEST(Parallel_Operations_MPI, Test_Max_2) {
       ASSERT_EQ(reference_max, global_max);
   }
   */
+  ASSERT_TRUE(true);
+}
+
+TEST(Parallel_Operations_MPI, Test_Max_4) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<std::vector<int>> global_matr;
+  const int n_size_matrix = 5, m_size_matrix = 8;
+
+  /*if (rank == 0) {
+      global_matr = getRandomMatrix(n_size_matrix, m_size_matrix);
+  }
+
+  int global_max;
+  global_max = getParallelOperations(global_matr, count_size_vector, "max");
+
+  if (rank == 0) {
+      int reference_max = getSequentialOperations(global_matr, "max");
+      ASSERT_EQ(reference_max, global_max);
+  }
+  */
+  ASSERT_TRUE(true);
+}
+
+TEST(Parallel_Operations_MPI, Test_Max_5) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<std::vector<int>> global_matr;
+  const int n_size_matrix = 5, m_size_matrix = 8;
+
+  /*if (rank == 0) {
+      global_matr = getRandomMatrix(n_size_matrix, m_size_matrix);
+  }
+
+  int global_max;
+  global_max = getParallelOperations(global_matr, count_size_vector, "max");
+
+  if (rank == 0) {
+      int reference_max = getSequentialOperations(global_matr, "max");
+      ASSERT_EQ(reference_max, global_max);
+  }
+  */
+  ASSERT_TRUE(true);
 }
 
 int main(int argc, char** argv) {
