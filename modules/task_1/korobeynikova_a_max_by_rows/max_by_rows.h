ef9b7ed7c59bdae1a6de7db6d02e36bade2c229f
wellcome2AD <iuuyyttttt@mail.ru>
2022-12-18 17:05:29 +0300

    Initial Commit

---
+++ modules/task_1/korobeynikova_a_max_by_rows/max_by_rows.h
@@ -0,0 +1,40 @@
+#pragma once
+// Copyright 2022 Korobeynikova Alisa
+#ifndef MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+#define MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+
+#include <vector>
+
+template<typename T>
+class Matrix
+{
+private:
+    std::vector<T> data;
+    int n, m;
+public:
+    Matrix(int _n, int _m) : data(std::vector<T>(_n* _m)), n(_n), m(_m) {}
+    int GetN() const
+    {
+        return n;
+    }
+    int GetM() const
+    {
+        return m;
+    }
+    std::vector<T> GetData()
+    {
+        return data;
+    }
+    T* operator[](int pos)
+    {
+        return data.data() + pos * m;
+    }
+};
+
+
+std::string vtos(std::vector<int>);
+Matrix<int> getRandomMatrix(int n, int m);
+std::vector<int> getParallelOperation(Matrix<int> global_matr);
+int findMax(std::vector<int> vec);
+
+#endif  // MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
