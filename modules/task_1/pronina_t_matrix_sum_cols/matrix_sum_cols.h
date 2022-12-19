e7a24813bdd48979a6b416cb9a48ce6068c82da6
TupoaMudille <jeffersonia772@gmail.com>
2022-12-20 00:24:00 +0300

    style fix

--- modules/task_1/pronina_t_matrix_sum_cols/matrix_sum_cols.h
+++ modules/task_1/pronina_t_matrix_sum_cols/matrix_sum_cols.h
@@ -1,6 +1,7 @@
 // Copyright 2022 Pronina Tatiana
-#ifndef MODULES_TASK_1_PRONINA_T_MATRIX_SUM_COLS_MATRIX_SUM_COLS_H_  
-#define MODULES_TASK_1_PRONINA_T_MATRIX_SUM_COLS_MATRIX_SUM_COLS_H_  
+#ifndef MODULES_TASK_1_PRONINA_T_MATRIX_SUM_COLS_MATRIX_SUM_COLS_H_
+#define MODULES_TASK_1_PRONINA_T_MATRIX_SUM_COLS_MATRIX_SUM_COLS_H_
+
 int* CreateRandMatrix(int rows, int cols);
 int* LinearMetod(int* Matrix, int rows, int cols);
 int* MPIMethod(int* Matrix_init, int rs, int cs);
