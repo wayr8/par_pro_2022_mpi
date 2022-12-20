dd093579924a5829f78d9d260c459e5ef444d15c
UAlex322 <epicalex2015@gmail.com>
2022-12-20 13:43:56 +0300

    Fix path name

--- modules/task_2/ustinov_a_simple_iteration/smpl_iter.h
+++ modules/task_2/ustinov_a_simple_iteration/smpl_iter.h
@@ -1,6 +1,6 @@
 // Copyright 2022 Ustinov A.
-#ifndef MODULES_TASK_2_USTINOV_A_SIMPLE_ITERATION_METHOD_SMPL_ITER_H_
-#define MODULES_TASK_2_USTINOV_A_SIMPLE_ITERATION_METHOD_SMPL_ITER_H_
+#ifndef MODULES_TASK_2_USTINOV_A_SIMPLE_ITERATION_SMPL_ITER_H_
+#define MODULES_TASK_2_USTINOV_A_SIMPLE_ITERATION_SMPL_ITER_H_
 
 #include <vector>
 
@@ -22,4 +22,4 @@ std::vector<double> matrix_vector_product(
         int n);
 std::vector<double> get_random_vector(size_t n);
 std::vector<double> get_random_stable_matrix(size_t n);
-#endif  // MODULES_TASK_2_USTINOV_A_SIMPLE_ITERATION_METHOD_SMPL_ITER_H_
+#endif  // MODULES_TASK_2_USTINOV_A_SIMPLE_ITERATION_SMPL_ITER_H_
