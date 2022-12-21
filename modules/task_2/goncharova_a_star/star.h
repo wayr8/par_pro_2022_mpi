b632dd4640be29c5f5b3aef5ef19d5be0e522313
GoncharovaAnna <38956075+GoncharovaAnna@users.noreply.github.com>
2022-12-22 01:32:09 +0300

    Revert "Lab 2"
    This reverts commit 58e86b33501a0eaad53fc0160c1a0244f4643ba4.

--- modules/task_2/goncharova_a_star/star.h
+++ modules/task_2/goncharova_a_star/star.h
@@ -1,5 +1,4 @@
 // Copyright Anna Goncharova
-
 #ifndef ONEDRIVE___________GITHUB_PAR_PRO_2022_MPI_MODULES_TASK_2_GONCHAROVA_A_STAR_STAR_H_
 #define ONEDRIVE___________GITHUB_PAR_PRO_2022_MPI_MODULES_TASK_2_GONCHAROVA_A_STAR_STAR_H_
 #include <mpi.h>
