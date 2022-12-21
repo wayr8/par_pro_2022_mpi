9fec9c0a0aec88a2ce2b911c519ea1ae02bd47d3
FirstStep0 <75475364+FirstStep0@users.noreply.github.com>
2022-12-21 16:33:35 +0300

    bug fixes 2

--- modules/task_1/shokurov_d_check_order/check_order.h
+++ modules/task_1/shokurov_d_check_order/check_order.h
@@ -1,15 +1,13 @@
 // Copyright 2022 Shokurov Daniil
-#ifndef MODULES_TASK_1_SHOKUROV_D_CHECK_ORDER_CHECK_ORDER_H
-#define MODULES_TASK_1_SHOKUROV_D_CHECK_ORDER_CHECK_ORDER_H
+#ifndef MODULES_TASK_1_SHOKUROV_D_CHECK_ORDER_CHECK_ORDER_H_
+#define MODULES_TASK_1_SHOKUROV_D_CHECK_ORDER_CHECK_ORDER_H_
 
 #include <vector>
 #include <string>
 
-using namespace std;
-
-void get_random_string(string& str, size_t n);
-void scatter_string(string& str1);
-int check_order_single_process(size_t n, string& a, string& b);
-void addNull(string& str, int count);
-int getOrder(string& str1, string& str2);
-#endif  // MODULES_TASK_1_SHOKUROV_D_CHECK_ORDER_CHECK_ORDER_H
+std::string get_random_string(size_t n);
+void scatter_string(std::string str1);
+int check_order_single_process(size_t n, std::string a, std::string& b);
+std::string addNull(std::string str, int count);
+int getOrder(std::string& str1, std::string& str2);
+#endif  // MODULES_TASK_1_SHOKUROV_D_CHECK_ORDER_CHECK_ORDER_H_
