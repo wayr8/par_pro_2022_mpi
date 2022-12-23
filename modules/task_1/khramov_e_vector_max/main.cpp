e07263de1ac3627c6d0b365acd9e2a6fefbd2c53
khramov23 <7962519971@ya.ru>
2022-12-23 19:49:57 +0300

    add 5th test

--- modules/task_1/khramov_e_vector_max/main.cpp
+++ modules/task_1/khramov_e_vector_max/main.cpp
@@ -68,6 +68,22 @@ TEST(Parallel_Operations_MPI, Test_Vector_Max_1_million_elements) {
     }
 }
 
+TEST(Parallel_Operations_MPI, Test_Vector_Max_3_million_elements) {
+    int rank, vec_size = 3000000;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::vector<int> random_vector;
+
+    if (rank == 0) random_vector = getRandomVector(vec_size);
+
+    int max_sequential;
+    int max_parallel = getMaxParallel(random_vector, vec_size);
+
+    if (rank == 0) {
+        max_sequential = getMax(random_vector);
+        ASSERT_EQ(max_sequential, max_parallel);
+    }
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
