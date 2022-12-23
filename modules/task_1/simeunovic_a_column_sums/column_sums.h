ae3c9f187c066fe04bfefa380aa20a09a05f7084
aleksandarsmnvc <36212895+aleksandarsmnvc@users.noreply.github.com>
2022-12-24 02:34:56 +0300

    Update column_sums.h

--- modules/task_1/simeunovic_a_column_sums/column_sums.h
+++ modules/task_1/simeunovic_a_column_sums/column_sums.h
@@ -4,11 +4,11 @@
 #include<vector>
 void DoWork(std::vector<int>* a, std::vector<int>* b);
 void CreateRandomMatrix(std::vector<int>* matrix, int row_num, int column_num);
-std::vector<int>SequencallSum(std::vector<int>matrix, int row_num, int column_num);
+std::vector<int>SequencallSum(std::vector<int>* matrix, int row_num, int column_num);
 void ColumnSumsParallel(int ProcRank, int ProcSize, std::vector<int>* pSendInd, std::vector<int>* pSendNum
 , std::vector<int>* result, const std::vector<int>& pProcColumns
 , std::vector<int>* pProcResults, int row_num, int column_num, int ColumnNum);
-void ColumnSumsSequenceally(int ProcRank, int ProcSize, const std::vector<int>& pProcColumns
+void ColumnSumsSequenceally(int ProcRank, int ProcSize, const std::vector<int>* pProcColumns
 , std::vector<int>* pProcResults, int row_num, int ColumnNum);
 void ProcessInitialization(int ProcRank, int ProcSize, std::vector<int>* matrix, std::vector<int>* results
 , std::vector<int>* pProcColumns, std::vector<int>* pProcResults, int* row_num, int* column_num, int* ColumnNum);
