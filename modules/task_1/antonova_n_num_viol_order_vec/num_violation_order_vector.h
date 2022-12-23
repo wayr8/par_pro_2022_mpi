f3ddd0278220fddc2d88284ba0a033dfc4e961e3
AnastasyaAntonova <antonova0506@icloud.com>
2022-12-23 16:00:02 +0300

    style fix

--- modules/task_1/antonova_n_num_viol_order_vec/num_violation_order_vector.h
+++ modules/task_1/antonova_n_num_viol_order_vec/num_violation_order_vector.h
@@ -1,6 +1,6 @@
 // Copyright 2022 Anastasya Anastasya
-#ifndef MODULES_TASK_1_ZINKOV_NUM_VIOLATION_ORDER_VECTOR_NUM_VIOLATION_ORDER_VECTOR_H_
-#define MODULES_TASK_1_ZINKOV_NUM_VIOLATION_ORDER_VECTOR_NUM_VIOLATION_ORDER_VECTOR_H_
+#ifndef MODULES_TASK_1_ANTONOVA_N_NUM_VIOL_ORDER_VEC_NUM_VIOLATION_ORDER_VECTOR_H_
+#define MODULES_TASK_1_ANTONOVA_N_NUM_VIOL_ORDER_VEC_NUM_VIOLATION_ORDER_VECTOR_H_
 
 #include <mpi.h>
 #include <vector>
@@ -9,4 +9,4 @@ std::vector<int> getRandomVector(int  sz);
 
 int getNumViolationOrderVector(std::vector<int> vec);
 int getNumViolationOrderVectorParallel(std::vector<int> global_vec, int count_size_vector);
-#endif  // MODULES_TASK_1_ZINKOV_NUM_VIOLATION_ORDER_VECTOR_NUM_VIOLATION_ORDER_VECTOR_H_
+#endif  // MODULES_TASK_1_ANTONOVA_N_NUM_VIOL_ORDER_VEC_NUM_VIOLATION_ORDER_VECTOR_H_
