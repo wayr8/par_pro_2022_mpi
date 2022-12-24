a07ce58c7d4031f683dc5ad902c8a91c8f2860fb
Kate <selestinka@com>
2022-12-24 17:55:03 +0300

    commit

--- modules/task_1/gosteeva_e_matrix_max/matrix_max.h
+++ modules/task_1/gosteeva_e_matrix_max/matrix_max.h
@@ -1,17 +1,16 @@
+  // Copyright 2022 Gosteeva Ekaterina
+#ifndef MODULES_TASK_1_GOSTEEVA_E_MATRIX_MAX_MATRIX_MAX_H_
+#define MODULES_TASK_1_GOSTEEVA_E_MATRIX_MAX_MATRIX_MAX_H_
 
-// Copyright 2022 Gosteeva Ekaterina
-#ifndef MODULES_TASK_1_GOSTEEVA_E_MATRIX_MAX_MATRIX_MAX_H
-#define MODULES_TASK_1_GOSTEEVA_E_MATRIX_MAX_MATRIX_MAX_H
-
-#include <vector>
 #include <mpi.h>
+#include <vector>
 #include <algorithm>
 #include <iostream>
 #include <random>
 #include <string>
 
 std::vector<int> GetRandomMatrix(int matrix_size);
-int GetMatrixMaxSequential(std::vector<int> &matrix);
-int GetMatrixMaxParralel(std::vector<int> &matrix, const int size);
+int GetMatrixMaxSequential(const std::vector<int> &matrix);
+int GetMatrixMaxParralel(const std::vector<int> &matrix, const int size);
 
-#endif // MODULES_TASK_1_GOSTEEVA_E_MATRIX_MAX_MATRIX_MAX_H
+#endif  // MODULES_TASK_1_GOSTEEVA_E_MATRIX_MAX_MATRIX_MAX_H_
