26254e07f88ea295da68664b7fc791adea35051e
korgi <igrok12005@yandex.ru>
2022-12-24 14:44:25 +0300

    task_2 0.8

--- modules/task_2/kosterin_a_gaus_lent_horiz/main.cpp
+++ modules/task_2/kosterin_a_gaus_lent_horiz/main.cpp
@@ -14,7 +14,7 @@ double **arr(int size) {
   for (int i = 0; i < size; i++) {
     ar[i] = new double[size];
     for (int j = 0; j < size; j++) {
-      double ran = static_cast<double>((gen() % 100));
+      double ran = static_cast<double>((gen() % 200));
       ar[i][j] = ran + 1;
     }
   }
