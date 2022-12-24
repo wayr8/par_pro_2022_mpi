7a0e2784a5a2901ecca8ca47dc80aed2f8efb1de
qooju <84714244+qooju@users.noreply.github.com>
2022-12-24 16:06:08 +0300

    Delete Reduce.h

--- modules/task_2/bikova_a_reduce/Reduce.h
+++
@@ -1,7 +0,0 @@
-#ifndef REDUCE
-#define REDUCE
-#include<stdafx.h>
-#include<mpi.h>
-void DoWork(MPI_Op op);
-void Reduce(int* sendbuf, int* recvbuf, int count, MPI_Datatype type, MPI_Op op, int root, MPI_Comm comm);
-#endif
