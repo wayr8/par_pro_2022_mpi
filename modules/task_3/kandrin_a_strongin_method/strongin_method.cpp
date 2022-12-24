0af97ffbb3a76cdd16f90a3fd3c4e3b8611651ed
Alexey Kandrin <adkandrin@gmail.com>
2022-12-24 14:50:07 +0300

    реализация через сортировку массива точек изменена на реализацию через структуру segment.

--- modules/task_3/kandrin_a_strongin_method/strongin_method.cpp
+++ modules/task_3/kandrin_a_strongin_method/strongin_method.cpp
@@ -8,17 +8,28 @@
 #include <cmath>
 #include <utility>
 
+namespace {
+struct Segment {
+  double begin;
+  double end;
+};
+}
+
 // calculate the "m" estimate of the Lipschitz constant:
-// M = max{1 <= i <= n}(abs((Z_{i} - Z_{i-1}) / (Y_{i} - Y_{i - 1}))
+// M = max{1 <= i <= n}(abs((Z_{i}.end - Z_{i}.begin) / (Y_{i}.end - Y_{i}.begin))
 // m = {1 if M = 0, r*M if M > 0}, where r > 1 - parameter,
-// Z_{i} = f(Y_{i})
-double Calculate_m(Function&& f, const std::vector<double>& y, const double r) {
+// Z_{i}.begin = f(Y_{i}.begin)
+// Z_{i}.end = f(Y_{i}.end)
+double Calculate_m(Function&& f, const std::vector<Segment>& y, const double r) {
   assert(r > 1);
 
   double M = 0;
-  for (int i = 1; i < y.size(); ++i) {
-    const double zDif = f(y.at(i)) - f(y.at(i - 1));
-    const double yDif = y.at(i) - y.at(i - 1);
+  for (int i = 0; i < y.size(); ++i) {
+    const double y_begin = y.at(i).begin;
+    const double y_end = y.at(i).end;
+
+    const double zDif = f(y_end) - f(y_begin);
+    const double yDif = y_end - y_begin;
     const double currentMax = std::abs(zDif / yDif);
     if (currentMax > M) {
       M = currentMax;
@@ -32,13 +43,17 @@ double Calculate_m(Function&& f, const std::vector<double>& y, const double r) {
 // R(i) = m(Y_{i} - Y_{i - 1}) + sqr(Z_{i} - Z_{i - 1}) / m(Y_{i} - Y{i - 1}) -
 // 2(Z_{i} + Z_{i-1}), where m = Calculate_M(...) (see above),
 // Z_{i} = f(Y_{i})
-int CalculateIndexOfMaxR(Function&& f, const std::vector<double>& y,
+int CalculateIndexOfMaxR(Function&& f, const std::vector<Segment>& y,
                          const double m) {
   std::pair<double, int> maxR_Index(-1, -1);
-  for (int i = 1; i < y.size(); ++i) {
-    const double yDif = y.at(i) - y.at(i - 1);
-    const double zDif = f(y.at(i)) - f(y.at(i - 1));
-    const double zSum = f(y.at(i)) + f(y.at(i - 1));
+
+  for (int i = 0; i < y.size(); ++i) {
+    const double y_begin = y.at(i).begin;
+    const double y_end = y.at(i).end;
+
+    const double yDif = y_end - y_begin;
+    const double zDif = f(y_end) - f(y_begin);
+    const double zSum = f(y_end) + f(y_begin);
     const double R = m * yDif + zDif * zDif / (m * yDif) - 2 * zSum;
 
     double& maxR = maxR_Index.first;
@@ -49,25 +64,29 @@ int CalculateIndexOfMaxR(Function&& f, const std::vector<double>& y,
       maxIndex = i;
     }
   }
+
   return maxR_Index.second;
 }
 
 // Get minimum of function f in [a; b]
 double GetMin(Function&& f, double a, double b, double epsilon) {
-  std::vector<double> y = {a, b};
+  std::vector<Segment> y = {Segment{a, b}};
 
   const double r = 2.0;
 
   while (true) {
-    std::sort(y.begin(), y.end());
 
     const double m = Calculate_m(std::forward<Function>(f), y, r);
     int i = CalculateIndexOfMaxR(std::forward<Function>(f), y, m);
-    if (y.at(i) - y.at(i - 1) < epsilon) {
-      return f(y.at(i));
+    const auto& currentSegment = y.at(i);
+    const double y_begin = currentSegment.begin;
+    const double y_end = currentSegment.end;
+    if (y_end - y_begin < epsilon) {
+      return f(y_end);
     }
-    double yn = y.at(i - 1) + (y.at(i) - y.at(i - 1)) / 2 +
-                (f(y.at(i)) - f(y.at(i - 1))) / (2 * m);
-    y.push_back(yn);
+    double yn =
+        y_begin + (y_end - y_begin) / 2 + (f(y_end) - f(y_begin)) / (2 * m);
+    y.push_back(Segment{y_begin, yn});
+    y.at(i).begin = yn;
   }
 }
