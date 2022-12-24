015242bf02101b70ffba1565e607158de5843aeb
Тимур Хайретдинов <khairetdinovtimur123@yandex.ru>
2022-12-24 18:03:44 +0300

    task2 0.5

--- modules/task_2/khairetdinov_t_scatter/scatter.h
+++ modules/task_2/khairetdinov_t_scatter/scatter.h
@@ -9,10 +9,10 @@
 #include <functional>
 
 int Scatter(void* send_data, int send_count, MPI_Datatype send_datatype, void* recv_data,
-                  int recv_count, MPI_Datatype recv_datatype, int root, MPI_Comm communicator);
+            int recv_count, MPI_Datatype recv_datatype, int root, MPI_Comm communicator);
 int FasterScatter(void* send_data, int send_count, MPI_Datatype send_datatype, void* recv_data,
-  int recv_count, MPI_Datatype recv_datatype, int root, MPI_Comm communicator);
+                  int recv_count, MPI_Datatype recv_datatype, int root, MPI_Comm communicator);
 int MyTest(std::vector<int> mas, std::function<int(void* , int , MPI_Datatype , void* ,
-  int , MPI_Datatype , int , MPI_Comm)> func);
+           int , MPI_Datatype , int , MPI_Comm)> func);
 
 #endif  // MODULES_TASK_2_KHAIRETDINOV_T_SCATTER_SCATTER_H_
