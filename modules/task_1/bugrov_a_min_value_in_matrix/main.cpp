f5c8961c8f847288f245880bec389fc75d2b7f3b
bugrov.and <bugrovandriy@yandex.ru>
2022-12-24 20:02:50 +0300

    a

--- modules/task_1/bugrov_a_min_value_in_matrix/main.cpp
+++ modules/task_1/bugrov_a_min_value_in_matrix/main.cpp
@@ -1,35 +1,90 @@
 #include <gtest/gtest.h>
 #include "./min_value_in_matrix.h"
 #include <gtest-mpi-listener.hpp>
-TEST(min_value_in_matrix, equals_value_no_reminder_2_proc)
+TEST(min_value_in_matrix, small_matr_no_reminder_2_proc)
 {
     int rank;
     int min_value1, min_value2;
     const int size = 500;
     int matrix[size];
-    int min_values[2];
+    const int proc_num = 2;
+    int min_values[proc_num];
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 0) {
         create_random_values(matrix, size);
         min_value1 = seq_min_value(matrix, size);
     }
-    par_min_value(matrix, size, 2, min_values);
+    par_min_value(matrix, size, proc_num, min_values);
+    min_value1 = min_values[0];
+    EXPECT_EQ(min_value2, min_value1);
 }
-TEST(min_value_in_matrix, random_values_no_reminder_2_proc)
+TEST(min_value_in_matrix, middle_matr_no_reminder_2_proc)
 {
-
+    int rank;
+    int min_value1, min_value2;
+    const int size = 25000;
+    int matrix[size];
+    const int proc_num = 2;
+    int min_values[proc_num];
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        create_random_values(matrix, size);
+        min_value1 = seq_min_value(matrix, size);
+    }
+    par_min_value(matrix, size, proc_num, min_values);
+    min_value1 = min_values[0];
+    EXPECT_EQ(min_value2, min_value1);
 }
-TEST(min_value_in_matrix, random_values_coprime_integers_3_proc)
+TEST(min_value_in_matrix, big_matr_coprime_integers_3_proc)
 {
-
+    int rank;
+    int min_value1, min_value2;
+    const int size = 1000000;
+    int matrix[size];
+    const int proc_num = 3;
+    int min_values[proc_num];
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        create_random_values(matrix, size);
+        min_value1 = seq_min_value(matrix, size);
+    }
+    par_min_value(matrix, size, proc_num, min_values);
+    min_value1 = min_values[0];
+    EXPECT_EQ(min_value2, min_value1);
 }
-TEST(min_value_in_matrix, random_values_coprime_integers_10_proc)
+TEST(min_value_in_matrix, big_matr_coprime_integers_10_proc)
 {
-
+    int rank;
+    int min_value1, min_value2;
+    const int size = 1000009;
+    int matrix[size];
+    const int proc_num = 10;
+    int min_values[proc_num];
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        create_random_values(matrix, size);
+        min_value1 = seq_min_value(matrix, size);
+    }
+    par_min_value(matrix, size, proc_num, min_values);
+    min_value1 = min_values[0];
+    EXPECT_EQ(min_value2, min_value1);
 }
-TEST(min_value_in_matrix, random_values_1_proc)
+TEST(min_value_in_matrix, big_matr_1_proc)
 {
-
+    int rank;
+    int min_value1, min_value2;
+    const int size = 1000009;
+    int matrix[size];
+    const int proc_num = 1;
+    int min_values[proc_num];
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        create_random_values(matrix, size);
+        min_value1 = seq_min_value(matrix, size);
+    }
+    par_min_value(matrix, size, proc_num, min_values);
+    min_value1 = min_values[0];
+    EXPECT_EQ(min_value2, min_value1);
 }
 
 int main(int argc, char** argv) {
