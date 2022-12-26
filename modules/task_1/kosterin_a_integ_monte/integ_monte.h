f8c46e945f35b566ba2fe28e272e2685af9b407f
StJi <denproko@yandex.ru>
2022-12-26 17:19:25 +0300

    Revert "Revert "clear""
    This reverts commit 79ce21933677af8dd77afe30963abf82aef23eb6.

--- modules/task_1/kosterin_a_integ_monte/integ_monte.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2022 Kosterin Alexey
-#ifndef MODULES_TASK_1_KOSTERIN_A_INTEG_MONTE_INTEG_MONTE_H_
-#define MODULES_TASK_1_KOSTERIN_A_INTEG_MONTE_INTEG_MONTE_H_
-
-double monteCarlo(int low, int high, int count, double (*f)(double));
-
-#endif  // MODULES_TASK_1_KOSTERIN_A_INTEG_MONTE_INTEG_MONTE_H_
