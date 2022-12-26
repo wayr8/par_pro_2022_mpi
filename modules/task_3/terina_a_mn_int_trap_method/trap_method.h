c9bc571233bd17d09ccfd7a2ea4554c8aeea9a82
imterinaa <84046726+imterinaa@users.noreply.github.com>
2022-12-26 23:45:39 +0300

    task_3 v1

---
+++ modules/task_3/terina_a_mn_int_trap_method/trap_method.h
@@ -0,0 +1,21 @@
+// Copyright 2022 Terina Alina
+#ifndef MODULES_TASK_3_TERINA_A_MN_INT_TRAP_METHOD_TRAP_H_
+#define MODULES_TASK_3_TERINA_A_MN_INT_TRAP_METHOD_TRAP_H_
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <mpi.h>
+#include <string>
+#include <vector>
+
+int getLocalSize(int rank, int count_sz, int remain);
+double First(std::vector<double> coefs);
+double Second(std::vector<double> coefs);
+std::vector<double> getRandomVector(int sz);
+double ParInt(std::vector<double> first_c, std::vector<double> second_c,
+    double (*PodIntF)(std::vector<double>), int n);
+double OrdinaryInt(std::vector<double> first_c, std::vector<double> second_c,
+    double (*PodIntF)(std::vector<double>), int n);
+
+#endif // MODULES_TASK_3_TERINA_A_MN_INT_TRAP_METHOD_TRAP_H_
