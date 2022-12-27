9868c929bfb1dff8ae66cddde51cda0bfdf0e61a
Vladimir <71398553+Vo-one@users.noreply.github.com>
2022-12-27 18:34:11 +0300

    first

--- modules/task_1/elistratov_v_letter_count/main.cpp
+++ modules/task_1/elistratov_v_letter_count/main.cpp
@@ -30,6 +30,19 @@ TEST(Parallel_Count_Letters_MPI, Size_1) {
   }
 }
 
+TEST(Parallel_Count_Letters_MPI, Size_25) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int elements_count = 25;
+  std::string result;
+  if (rank == 0) result = CreateRandomString(elements_count);
+  int sum = calculateCountParallel(result, elements_count);
+  if (rank == 0) {
+    int control_sum = calculateCountSequental(result);
+    ASSERT_EQ(control_sum, sum);
+  }
+}
+
 TEST(Parallel_Count_Letters_MPI, Size_100) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
