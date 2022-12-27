a753c665a8e5132f2709508c30227cae276a0e02
aleksandarsmnvc <36212895+aleksandarsmnvc@users.noreply.github.com>
2022-12-27 18:28:21 +0300

    Update slepping_barber.h

--- modules/task_2/simeunovic_a_slepping_barber/slepping_barber.h
+++ modules/task_2/simeunovic_a_slepping_barber/slepping_barber.h
@@ -7,6 +7,7 @@ void DoBarberLoop(int n, int ProcSize, int ProcRank);
 void Cutting_Hair(int client);
 void ClientLoop(int ProcRank);
 void Sleeping(int* mutex, bool* thread_running);
-void Empty_Buffer(bool* working, int* count);
-void Checking(int ProcRank);
+// void Empty_Buffer(bool* working, int* count);
+// void Checking(int ProcRank);
+bool AnyWorks(bool* procesess, int ProcSize);
 #endif  // MODULES_TASK_2_SIMEUNOVIC_A_SLEPPING_BARBER_SLEPPING_BARBER_H_
