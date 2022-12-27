6393ce3fef8291b18b49665e4f96c0cab1fa0cc1
Kate <selestinka@com>
2022-12-27 18:18:35 +0300

    c2

--- modules/task_2/gosteeva_e_producers_consumers/produsers_consumers.cpp
+++ modules/task_2/gosteeva_e_producers_consumers/produsers_consumers.cpp
@@ -12,7 +12,6 @@ int GetAccess(int num) {
     MPI_Status MPI_status;
     while (num > 0) {
         MPI_Recv(&req, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &MPI_status);
-        
         switch (MPI_status.MPI_TAG) {
         case peq_from_prod_tag:
             res = 0;
