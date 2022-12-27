e6f20ce05457f30e54acbd02b97b92c7092e8e9b
Пашина Алина <nice.alina28@mail.ru>
2022-12-28 01:31:55 +0300

    commit 2.0

--- modules/task_2/pashina_a_bubble_sort/Main.cpp
+++ modules/task_2/pashina_a_bubble_sort/Main.cpp
@@ -1,68 +1,96 @@
 // Copyright 2022 Pashina Alina
 
 #include <gtest/gtest.h>
-#include <mpi.h>
-#include <iostream>
 #include "../../../modules/task_2/pashina_a_bubble_sort/pashina_a_bubble_sort.h"
 #include <gtest-mpi-listener.hpp>
 
-TEST(generatearr_MPI, can_create_arr) {
-  int t_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &t_rank);
-  if (t_rank == 0) {
-    int n = 3;
-    ASSERT_NO_THROW(generateNewArr(n));
+TEST(doParallel_BubbleS, par_test_2) {
+  int th_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &th_rank);
+  int mysize = 2;
+  int* start_arr = new int[mysize];
+  int* ref_array;
+  start_arr = generateNewArr(mysize);
+  int* res_arr_Par = doParallel_BubbleS(start_arr, mysize);
+  if (th_rank == 0) {
+    int* arr = bubble_s_with_return(start_arr, mysize);
+    ASSERT_EQ(res_arr_Par, arr);
   }
 }
 
-TEST(doParallel_BubbleOddEven_MPI, can_do_para4) {
-  int t_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &t_rank);
-  int n = 4;
-  double *a = new double[n];
-  a = generateNewArr(n);
-  double *b = new double[n];
-  ASSERT_NO_THROW(doParallel_BubbleOddEven(n, a, 0, MPI_COMM_WORLD));
-}
-TEST(generatearr_MPI, can_create_img) {
-  int t_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &t_rank);
-  int n = 4;
-  double *a = new double[n];
-  a = generateNewArr(n);
-  double *b = new double[n];
-  if (t_rank == 0) ASSERT_NO_THROW(typical_bubble_s(a, n));
+TEST(doParallel_BubbleOddEven_MPI, par_test_11) {
+  int th_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &th_rank);
+  int mysize = 11;
+  int* start_arr = new int[mysize];
+  int* ref_array;
+
+  start_arr = generateNewArr(mysize);
+  int* res_arr_Par = doParallel_BubbleS(start_arr, mysize);
+
+  if (th_rank == 0) {
+    int* arr = bubble_s_with_return(start_arr, mysize);
+    ASSERT_EQ(res_arr_Par, arr);
+  }
 }
 
-TEST(doParallel_BubbleOddEven_MPI, can_do_paral2) {
-  int t_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &t_rank);
-  int n = 2;
-  double *a = new double[n];
-  a = generateNewArr(n);
-  double *b = new double[n];
-  ASSERT_NO_THROW(doParallel_BubbleOddEven(n, a, 0, MPI_COMM_WORLD));
-}
-TEST(doParallel_BubbleOddEven_MPI, can_do_paral5) {
-  int t_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &t_rank);
-  int n = 5;
-  double *a = new double[n];
-  a = generateNewArr(n);
-  double *b = new double[n];
-  ASSERT_NO_THROW(doParallel_BubbleOddEven(n, a, 0, MPI_COMM_WORLD));
+TEST(doParallel_BubbleOddEven_MPI, par_test_15) {
+  int th_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &th_rank);
+  int mysize = 11;
+  int* start_arr = new int[mysize];
+  int* ref_array;
+
+  start_arr = generateNewArr(mysize);
+  int* res_arr_Par = doParallel_BubbleS(start_arr, mysize);
+
+  if (th_rank == 0) {
+    int* arr = bubble_s_with_return(start_arr, mysize);
+    ASSERT_EQ(res_arr_Par, arr);
+  }
 }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  MPI_Init(&argc, &argv);
-  ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-  ::testing::TestEventListeners &listeners =
-      ::testing::UnitTest::GetInstance()->listeners();
+  TEST(doParallel_BubbleOddEven_MPI, par_test_20) {
+    int th_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &th_rank);
+    int mysize = 11;
+    int* start_arr = new int[mysize];
+    int* ref_array;
 
-  listeners.Release(listeners.default_result_printer());
-  listeners.Release(listeners.default_xml_generator());
+    start_arr = generateNewArr(mysize);
+    int* res_arr_Par = doParallel_BubbleS(start_arr, mysize);
 
-  listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-  return RUN_ALL_TESTS();
-}
+    if (th_rank == 0) {
+      int* arr = bubble_s_with_return(start_arr, mysize);
+      ASSERT_EQ(res_arr_Par, arr);
+    }
+  }
+  TEST(doParallel_BubbleOddEven_MPI, par_test_3) {
+    int th_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &th_rank);
+    int mysize = 3;
+    int* start_arr = new int[mysize];
+    int* ref_array;
+
+    start_arr = generateNewArr(mysize);
+    int* res_arr_Par = doParallel_BubbleS(start_arr, mysize);
+
+    if (th_rank == 0) {
+      int* arr = bubble_s_with_return(start_arr, mysize);
+      ASSERT_EQ(res_arr_Par, arr);
+    }
+  }
+
+  int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+  }
--- modules/task_2/pashina_a_bubble_sort/pashina_a_bubble_sort.h
+++ modules/task_2/pashina_a_bubble_sort/pashina_a_bubble_sort.h
@@ -1,14 +1,7 @@
 // Copyright 2022 Pashina Alina
 
 #pragma once
-#include <mpi.h>
-double *typical_bubble_s(double *our_arr_to_sort, int sizearr);
-double *generateNewArr(int al);
-int mconnect_sorting(int narr, double *ourin_arr);
-int mconnect(double *first_ar, int len_first_ar, double *sec_ar, int sec_ar_len,
-             double *ourfin);
-void Change_Places_MPI(int sizeL, double *arrL, int rankToExp, int recvrank,
-                       MPI_Comm comm);
-int start_sort_before_M(double *f_ar, int begining, int finish, double *s_ar);
-double *doParallel_BubbleOddEven(int len, double *myarr, int mainstart,
-                                 MPI_Comm comm);
+void typical_bubble_s(int *myarray, int mysize);
+int *generateNewArr(int al);
+int *doParallel_BubbleS(int *ourarr, int arr_size_in);
+int *bubble_s_with_return(int *arr, int n);
