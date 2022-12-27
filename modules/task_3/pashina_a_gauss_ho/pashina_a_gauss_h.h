d69de5a6a5b446101d6f22e4679eef884fd97759
Пашина Алина <nice.alina28@mail.ru>
2022-12-27 19:24:31 +0300

    commit

---
+++ modules/task_3/pashina_a_gauss_ho/pashina_a_gauss_h.h
@@ -0,0 +1,12 @@
+// Copyright 2022 Pashina Alina
+#pragma once
+#include <vector>
+
+std::vector<unsigned char> generateNewImage(int rows_num, int columns_num);
+unsigned char getNewColor(const std::vector<unsigned char> &myimg, int startX,
+                          int startY, int rows_n, int columns_n);
+std::vector<unsigned char> gauss_filt(std::vector<unsigned char> mystart_image,
+                                      int rows_num, int columns_num);
+std::vector<unsigned char> DoParallel_fGauss(
+    std::vector<unsigned char> mystart_im, int rows_num, int columns_num);
+
