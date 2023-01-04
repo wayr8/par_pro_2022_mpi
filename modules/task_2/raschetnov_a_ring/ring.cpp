faf0c87d273f901f3654d88c1c0ff0e2184e16a3
alser3217 <raschetnov.alexei@gmail.com>
2023-01-04 22:20:54 +0300

    task_2

---
+++ modules/task_2/raschetnov_a_ring/ring.cpp
@@ -0,0 +1,49 @@
+// Copyright 2023 Raschetnov Alexei
+#include <mpi.h>
+#include <random>
+#include <iostream>
+#include "../../../modules/task_2/raschetnov_a_ring/ring.h"
+
+int getResult(int information, int size, int delta) {
+  for (int i = 0; i < size - 1; i++)
+    information += delta;
+  return information;
+}
+
+int SendRingParallel(int information, int count, MPI_Datatype datatype,
+                     int root, int delta, int tag, MPI_Comm comm) {
+  int size, rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);  
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (size == 1)
+    return information;
+  if (rank == root) {
+    MPI_Status status;
+    int dest = rank - 1;
+    if (dest < 0)
+      dest = size - 1;
+    MPI_Send(&information, 1, MPI_INT, (rank + 1) % size, 0, MPI_COMM_WORLD);
+
+    MPI_Recv(&information, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, &status);
+
+    return information;
+  }
+  else {
+    MPI_Status status;
+    int dest = rank - 1;
+    if (dest < 0)
+      dest = size - 1;
+    MPI_Recv(&information, 1, MPI_INT, dest, 0, MPI_COMM_WORLD, &status);
+    information += delta;
+
+    MPI_Send(&information, 1, MPI_INT, (rank + 1) % size, 0, MPI_COMM_WORLD);
+  }
+}
+
+int getRandomNumber(int leftborder, int rightborder) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distr(leftborder, rightborder);
+
+  return distr(gen);
+}
---
+++ modules/task_2/raschetnov_a_ring/ring.h
@@ -0,0 +1,12 @@
+// Copyright 2023 Raschetnov Alexei
+#ifndef MODULES_TASK_2_RASCHETNOV_A_RING_MPI_H_
+#define MODULES_TASK_2_RASCHETNOV_A_RING_MPI_H_
+
+#include <mpi.h>
+
+int SendRingParallel(int information, int count, MPI_Datatype datatype,
+                     int root, int delta, int tag, MPI_Comm comm);
+int getResult(int information, int size, int delta);
+int getRandomNumber(int leftborder, int rightborder);
+
+#endif  // MODULES_TASK_2_RASCHETNOV_A_RING_MPI_H_
