481b60b0dcbc295f07b19bfe30b8ac061c31554e
Anton Yarakhtin <71460313+1nanto@users.noreply.github.com>
2023-01-04 23:34:14 +0300

    i2

--- modules/task_2/yarakhtin_a_topology_torus/topology_torus.h
+++ modules/task_2/yarakhtin_a_topology_torus/topology_torus.h
@@ -1,22 +1,20 @@
 // Copyright 2022 Yarakhtin Anton
-#ifndef MODULES_TASK_2_YARAKHTIN_ANTON_TOPOLOGY_TORUS_TOPOLOGY_TORUS_H_
-#define MODULES_TASK_2_YARAKHTIN_ANTON_TOPOLOGY_TORUS_TOPOLOGY_TORUS_H_
+#ifndef MODULES_TASK_2_YARAKHTIN_A_TOPOLOGY_TORUS_TOPOLOGY_TORUS_H_
+#define MODULES_TASK_2_YARAKHTIN_A_TOPOLOGY_TORUS_TOPOLOGY_TORUS_H_
 
-#include <vector>
 #include <mpi.h>
+#include <vector>
 
-struct Vector2
-{
-	int x;
-	int y;
+struct Vector2 {
+    int x;
+    int y;
 };
 
-struct TorusElement
-{
-	Vector2 pos;
-	Vector2 size;
-	MPI_Comm comm_x;
-	MPI_Comm comm_y;
+struct TorusElement {
+    Vector2 pos;
+    Vector2 size;
+    MPI_Comm comm_x;
+    MPI_Comm comm_y;
 };
 
 Vector2 getCoordinates(int rank, const Vector2& size);
@@ -24,4 +22,4 @@ int getRank(const Vector2& coord, const Vector2& size);
 TorusElement torusCreate(const Vector2& size);
 void torusSend(void* buf, int count, MPI_Datatype datatype, int src, int dst, int tag, const TorusElement& tor);
 
-#endif  // MODULES_TASK_2_YARAKHTIN_ANTON_TOPOLOGY_TORUS_TOPOLOGY_TORUS_H_
+#endif  // MODULES_TASK_2_YARAKHTIN_A_TOPOLOGY_TORUS_TOPOLOGY_TORUS_H_
