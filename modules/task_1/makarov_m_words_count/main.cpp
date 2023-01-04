a1424c7433a6bf52c7b25d0087771ac74d8c62bf
m4karovms <mishamakarov2002@inbox.ru>
2023-01-04 13:22:29 +0300

    Add task 1

---
+++ modules/task_1/makarov_m_words_count/main.cpp
@@ -0,0 +1,53 @@
+// Copyright 2023 Makarov Mikhail
+#include <gtest/gtest.h>
+#include <vector>
+#include "./words_count.h"
+#include <gtest-mpi-listener.hpp>
+
+void RandomStringTest(int length) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::string randStr;
+    int seqWords, parWords;
+
+    if (rank == 0) {
+        randStr = getRandStr(length);
+        // std::cout << "randStr:\n{" << randStr << "}\n";
+
+        seqWords = countWordsSequential(randStr, length);
+        // std::cout << "seqWords = " << seqWords << "\n";
+    }
+
+    parWords = countWordsParallel(randStr, length);
+
+    if (rank == 0) {
+        // std::cout << "parWords = " << parWords << "\n";
+        EXPECT_EQ(seqWords, parWords);
+    }
+}
+
+TEST(MatMultHorA, Test_MatMultHorA_1) { RandomStringTest(10); }
+
+TEST(MatMultHorA, Test_MatMultHorA_2) { RandomStringTest(20); }
+
+TEST(MatMultHorA, Test_MatMultHorA_3) { RandomStringTest(50); }
+
+TEST(MatMultHorA, Test_MatMultHorA_4) { RandomStringTest(100); }
+
+TEST(MatMultHorA, Test_MatMultHorA_5) { RandomStringTest(50000); }
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners &listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_1/makarov_m_words_count/words_count.h
@@ -0,0 +1,19 @@
+// Copyright 2023 Makarov Mikhail
+#ifndef MODULES_TASK_1_MAKAROV_M_WORDS_COUNT_WORDS_COUNT_H_
+#define MODULES_TASK_1_MAKAROV_M_WORDS_COUNT_WORDS_COUNT_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <random>
+
+#define MY_ROOT (0)
+typedef unsigned long long ull;
+
+std::string getRandStr(int length);
+void printIntArr(int array[], int size);
+
+int countWordsSequential(std::string str, int length);
+int countWordsParallel(std::string str, int length);
+
+#endif  // MODULES_TASK_1_MAKAROV_M_WORDS_COUNT_WORDS_COUNT_H_
