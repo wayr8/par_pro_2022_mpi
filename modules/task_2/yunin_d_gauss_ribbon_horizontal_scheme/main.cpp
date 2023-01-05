467e2407a35ee703ff60fc59e091cddf04f7dc0f
DDYunin <daniilunin2002@yandex.ru>
2023-01-05 16:31:26 +0300

    i am trying 3

--- modules/task_2/yunin_d_gauss_ribbon_horizontal_scheme/main.cpp
+++ modules/task_2/yunin_d_gauss_ribbon_horizontal_scheme/main.cpp
@@ -2,8 +2,9 @@
 #include <mpi.h>
 #include <gtest/gtest.h>
 #include <gtest-mpi-listener.hpp>
-#include "./gauss_ribbon_horizontal_scheme.h"
 #include <vector>
+#include "./gauss_ribbon_horizontal_scheme.h"
+
 
 TEST(Solving_Method_Gauss_Ribbon_Horizontal_Scheme, Test_matrix_100x100) {
     int rank;
