518e5c8ae14a5d56cd5545837037a3d2fb81542b
Sukharev <artem200340447@gmail.com>
2023-01-05 16:26:14 +0300

    add_two_dim_opt_and_all_tests

--- modules/task_3/sukharev_a_gsa_parall_by_charact/main.cpp
+++ modules/task_3/sukharev_a_gsa_parall_by_charact/main.cpp
@@ -1,5 +1,6 @@
 // Copyright 2022 Sukharev Artem
 #include <gtest/gtest.h>
+#include <mpi.h>
 
 #include <functional>
 #include <iostream>
@@ -7,16 +8,107 @@
 #include <gtest-mpi-listener.hpp>
 #include "./glob_opt.h"
 
-TEST(Glob_opt_parall_by_characteristics, Test_1) {
+constexpr double SIGMA = 0.01;
+
+TEST(Glob_opt_parall_by_characteristics, Test_Independent_Coordinates) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   std::function<double(double, double)> func = [](double x,
                                                   double y) -> double {
-    // return sin(x)+cos(y); // -1 по y[0,10](x==0)
-    // return sin(x)+y*sin(y*2+2)-cos(2*y); //-6.7306 по y[-3,10](x==0)
-    return sin(x) + sin(y * 20 + 2) / y - 5 * y * cos(3 * y + 10); // -5.21608 по y[0.25,2.5](x==0)
+    return sin(x) + sin(y * 20 + 2) / y - 5 * y * cos(3 * y + 10);
   };
-  double lbX = 0, rbX = 10;
+  double lbX = -3, rbX = 3;
   double lbY = 0.25, rbY = 2.5;
-  std::cout << one_dimension_gsa(func, 0, lbY, rbY, 0.001) << "\n";
+  double errorXY = 0.01;
+  double expectedRes = -6.2160;
+  if (rank == 0) {
+    double result = GSA(func, lbX, rbX, lbY, rbY, errorXY);
+    // std::cout << result << "\n";
+    ASSERT_TRUE(std::abs(result - expectedRes) < SIGMA);
+  } else {
+    GSA(func, lbX, rbX, lbY, rbY, errorXY);
+  }
+}
+
+TEST(Glob_opt_parall_by_characteristics, Test_One_Extremum_Function) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::function<double(double, double)> func = [](double x,
+                                                  double y) -> double {
+    return (pow(x, 2) + pow(y, 2)) - 4.5;
+  };
+  double lbX = -2, rbX = 2;
+  double lbY = -2, rbY = 2;
+  double errorXY = 0.01;
+  double expectedRes = -4.5000;
+  if (rank == 0) {
+    double result = GSA(func, lbX, rbX, lbY, rbY, errorXY);
+    // std::cout << result << "\n";
+    ASSERT_TRUE(std::abs(result - expectedRes) < SIGMA);
+  } else {
+    GSA(func, lbX, rbX, lbY, rbY, errorXY);
+  }
+}
+
+TEST(Glob_opt_parall_by_characteristics, Test_Multi_Extremum_Function1) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::function<double(double, double)> func = [](double x,
+                                                  double y) -> double {
+    return sin(3 * x + 2 * cos(y)) + cos(pow((x + 1), 2) - 2 * y);
+  };
+  double lbX = -4, rbX = 2;
+  double lbY = -2, rbY = 3;
+  double errorXY = 0.01;
+  double expectedRes = -2.0000;
+  if (rank == 0) {
+    double result = GSA(func, lbX, rbX, lbY, rbY, errorXY);
+    // std::cout << result << "\n";
+    ASSERT_TRUE(std::abs(result - expectedRes) < SIGMA);
+  } else {
+    GSA(func, lbX, rbX, lbY, rbY, errorXY);
+  }
+}
+
+TEST(Glob_opt_parall_by_characteristics, Test_Multi_Extremum_Function2) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::function<double(double, double)> func = [](double x,
+                                                  double y) -> double {
+    return (y - 1) * (y + 1) *
+           (3 * sin(-x * 2) - x * cos(2 * x) - 2 * sin(5 * x));
+  };
+  double lbX = -1, rbX = 2.5;
+  double lbY = -1.5, rbY = 1.5;
+  double errorXY = 0.01;
+  double expectedRes = -5.5625;
+  if (rank == 0) {
+    double result = GSA(func, lbX, rbX, lbY, rbY, errorXY);
+    // std::cout << result << "\n";
+    ASSERT_TRUE(std::abs(result - expectedRes) < SIGMA);
+  } else {
+    GSA(func, lbX, rbX, lbY, rbY, errorXY);
+  }
+}
+
+TEST(Glob_opt_parall_by_characteristics, Test_One_Dimension) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::function<double(double, double)> func = [](double x,
+                                                  double y) -> double {
+    return y * sin(2 * y + 2) - cos(2 * y) + 10;
+  };
+  double lbX = 10, rbX = 10;
+  double lbY = -3, rbY = 10;
+  double errorXY = 0.01;
+  double expectedRes = 3.2694;
+  if (rank == 0) {
+    double result = GSA(func, lbX, rbX, lbY, rbY, errorXY);
+    // std::cout << result << "\n";
+    ASSERT_TRUE(std::abs(result - expectedRes) < SIGMA);
+  } else {
+    GSA(func, lbX, rbX, lbY, rbY, errorXY);
+  }
 }
 
 int main(int argc, char** argv) {
