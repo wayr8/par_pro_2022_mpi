b848e6e3279ffe2e4292b9ebe9994c50e764b645
Anna <annakul41@mail.ru>
2023-01-06 00:18:51 +0300

    First commit

---
+++ modules/task_1/kuleva_a_check_lexicographic_ordering/check_lexicographic_ordering.h
@@ -0,0 +1,14 @@
+// Copyright 2023 Kuleva Anna
+#ifndef MODULES_TASK_1_KULEVA_A_CHECK_LEXICOGRAPHIC_ORDERING_CHECK_LEXICOGRAPHIC_ORDERING 
+#define MODULES_TASK_1_KULEVA_A_CHECK_LEXICOGRAPHIC_ORDERING_CHECK_LEXICOGRAPHIC_ORDERING
+
+#include <vector>
+#include <string>
+
+std::string getRandomString(const int strLen);
+int getParallelOperations(std::string global_str_inp1, std::string global_str_inp2,
+                        size_t global_len);
+int getSequentialOperations(std::string str_inp1,
+                        std::string str_inp2);
+
+#endif  // MODULES_TASK_1_KULEVA_A_CHECK_LEXICOGRAPHIC_ORDERING_CHECK_LEXICOGRAPHIC_ORDERING
