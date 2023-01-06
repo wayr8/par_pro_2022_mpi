6b261c0cae2b3c6b10e783ccebaa0a2ebdfa3238
Лев Поляков <lev-polyackov@mail.ru>
2023-01-06 11:05:54 +0300

    polyackov_l_star_topology

--- modules/task_2/polyackov_l_star_topology/star_topology.cpp
+++ modules/task_2/polyackov_l_star_topology/star_topology.cpp
@@ -1,6 +1,43 @@
 // Copyright 2022 Polyackov Lev
 #include "../../../modules/task_2/polyackov_l_star_topology/star_topology.h"
 
+void MPI_Send_With_Star(void* message, int count, MPI_Datatype datatype, int from, int dest, int tag, MPI_Comm comm, std::vector<int>& path) {
+    path.clear();
+    if (from == dest) {
+        path.push_back(from);
+        path.push_back(dest);
+        return;
+    }
+
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank != from) {
+        if (rank == 0 && dest != 0) {
+            MPI_Status status;
+            MPI_Datatype* buf = new MPI_Datatype[count];
+
+            MPI_Recv(buf, count, datatype, from, tag, comm, &status);
+            path.push_back(from);
+            MPI_Send(buf, count, datatype, dest, tag, comm);
+            path.push_back(dest);
+            delete[] buf;
+            path.push_back(rank);
+        }
+        return;
+    } else {
+        if (from != 0 && dest != 0) {
+            path.push_back(rank);
+            MPI_Send(message, count, datatype, 0, tag, comm);
+        } else {
+            path.push_back(rank);
+            MPI_Send(message, count, datatype, dest, tag, comm);
+        }
+        return;
+    }
+}
+
 void MPI_Star_create(MPI_Comm* comm) {
     int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
